aada59abdacf5f5a0749bc1d40d34f11d5c5a5d0
Kalle <kalle.collan@zaibatsu.fi>
2021-01-30 00:27:31 +0200

    Fiddled around with a grid system. Nothing very smart or functional yet.

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField] private List<Node> adjacentNodes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetAdjacentNodes();
+        }
+    }
+
+    private void GetAdjacentNodes()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2))
+        {
+            if (hit.transform.CompareTag("Node"))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            }
+            else if (hit.transform.CompareTag("Wall"))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+            }
+
+            Debug.Log("Did Hit");
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, 2))
+        {
+            if (hit.transform.CompareTag("Node"))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.back) * hit.distance, Color.yellow);
+            }
+            else if (hit.transform.CompareTag("Wall"))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.back) * hit.distance, Color.red);
+            }
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 2))
+        {
+            if (hit.transform.CompareTag("Node"))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * hit.distance, Color.yellow);
+            }
+            else if (hit.transform.CompareTag("Wall"))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * hit.distance, Color.red);
+            }
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, 2))
+        {
+            if (hit.transform.CompareTag("Node"))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * hit.distance, Color.yellow);
+            }
+            else if (hit.transform.CompareTag("Wall"))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * hit.distance, Color.red);
+            }
+        }
+    }
+}
