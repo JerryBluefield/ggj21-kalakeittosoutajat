5ac556d7cbfc8a63927258f36205a71c7f213ad3
Jerry Blåfield <jerry.blafield@zaibatsu.fi>
2021-01-30 13:19:46 +0200

    Added menu scene, animations, fonts

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] MenuButtonController menuButtonController;
+    [SerializeField] Animator animator;
+    [SerializeField] AnimatorFunctions animatorFunctions;
+    [SerializeField] int thisIndex;
+
+    void Update()
+    {
+        if(menuButtonController.index == thisIndex)
+        {
+            animator.SetBool("isSelected", true);
+            if(Input.GetAxis("Submit") == 1)
+            {
+                animator.SetBool("isPressed", true);
+            }
+            else if (animator.GetBool("isPressed"))
+            {
+                animator.SetBool("isPressed", false);
+                animatorFunctions.disableOnce = true;
+            }
+        }
+        else
+        {
+            animator.SetBool("isSelected", false);
+        }
+    }
+}
---
+++ Assets/Scripts/MenuButtonController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonController : MonoBehaviour
+{
+    public int index;
+    [SerializeField] bool keyDown;
+    [SerializeField] int maxIndex;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(Input.GetAxis ("Vertical") != 0)
+        {
+            if (!keyDown)
+            {
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    if(index < maxIndex)
+                    {
+                        index++;
+                    } else 
+                    {
+                        index = 0;
+                    }
+                } 
+                else if(Input.GetAxis("Vertical") > 0)
+                {
+                    if(index > 0)
+                    {
+                        index--;
+                    } 
+                    else
+                    {
+                        index = maxIndex;
+                    }
+                }
+                keyDown = true;
+            }
+        } 
+        else
+        {
+            keyDown = false;
+        }
+    }
+}
