55be0da395d36c5320563094755a1e42df9084b4
TuomoO <tuomo.oksanen@zaibatsu.fi>
2021-01-30 13:06:57 +0200

    Multiplayer test scene

---
+++ Assets/Scripts/NetworkManagerKalakeitto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManagerKalakeitto : Mirror.NetworkManager
+{
+    public override void OnServerAddPlayer(Mirror.NetworkConnection conn)
+    {
+        Debug.Log("Mirror.OnServerAddPlayer");
+        GameObject player = Instantiate(new GameObject());
+        Mirror.NetworkServer.AddPlayerForConnection(conn, player);
+    }
+
+    public override void OnServerDisconnect(Mirror.NetworkConnection conn)
+    {
+        Debug.Log("Mirror.OnServerDisconnect");
+        base.OnServerDisconnect(conn);
+    }
+}
