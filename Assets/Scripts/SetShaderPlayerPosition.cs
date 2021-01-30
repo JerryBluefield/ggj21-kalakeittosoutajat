cf5193e4b0bd4ca2ceeca8419e96d26103d7324f
Juho Turpeinen <juho.turpeinen@zaibatsu.fi>
2021-01-30 13:52:28 +0200

    Better mist.

---
+++ Assets/Scripts/SetShaderPlayerPosition.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+[ExecuteInEditMode]
+public class SetShaderPlayerPosition : MonoBehaviour
+{
+    [SerializeField] private VisualEffect mistEffect;
+
+    private void Start()
+    {
+        mistEffect = GetComponentInChildren<VisualEffect>();
+    }
+
+    private void Update()
+    {
+        if (mistEffect != null)
+        {
+            mistEffect.SetVector3("_PlayerPosition", transform.position);
+        }
+    }
+}
