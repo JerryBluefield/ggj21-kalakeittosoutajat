de116a97ff8feda4f115a902fc897f8fc855e749
Juho Turpeinen <juho.turpeinen@zaibatsu.fi>
2021-01-31 12:52:14 +0200

    Revert "Update RandomActive.cs"
    This reverts commit df50343e1d39009d65a250ce2071d8c070ce59b8.

--- Assets/Scripts/RandomActive.cs
+++ Assets/Scripts/RandomActive.cs
@@ -1,22 +1,12 @@
 ﻿using UnityEngine;
 
-[ExecuteInEditMode]
 public class RandomActive : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] [Range(0f, 1f)] private float activeChance = 0.05f;
-    [SerializeField] private bool disableInEditor;
 
     private void Start()
     {
-        if (Application.isPlaying)
-        {
-            gameObject.SetActive(Random.value < activeChance);
-        }
-
-        if (!Application.isPlaying)
-        {
-            gameObject.SetActive(disableInEditor);
-        }
+        gameObject.SetActive(Random.value < activeChance);
     }
 }
