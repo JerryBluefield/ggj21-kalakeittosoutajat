44d8a23b1b00cf23d3ee8a5299980f98a0ab16dd
Juho Turpeinen <juho.turpeinen@zaibatsu.fi>
2021-01-31 13:03:59 +0200

    Disable shadows if far away.

---
+++ Assets/Scripts/DisableShadowsOnDemand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+[RequireComponent(typeof(HDAdditionalLightData))]
+public class DisableShadowsOnDemand : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float enableDistance;
+    private Transform mainCamera;
+    private HDAdditionalLightData light;
+    private Vector3 position;
+
+    private void Start()
+    {
+        mainCamera = Camera.main.transform;
+        light = GetComponent<HDAdditionalLightData>();
+        position = transform.position;
+    }
+
+    private void Update()
+    {
+        light.EnableShadows((position - mainCamera.position).sqrMagnitude < enableDistance * enableDistance);
+    }
+}
