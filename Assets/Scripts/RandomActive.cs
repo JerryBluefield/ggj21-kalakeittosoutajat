df50343e1d39009d65a250ce2071d8c070ce59b8
Juho Turpeinen <juho.turpeinen@zaibatsu.fi>
2021-01-31 12:47:48 +0200

    Update RandomActive.cs

--- Assets/Scripts/RandomActive.cs
+++ Assets/Scripts/RandomActive.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 
+[ExecuteInEditMode]
 public class RandomActive : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] [Range(0f, 1f)] private float activeChance = 0.05f;
+    [SerializeField] private bool disableInEditor;
 
     private void Start()
     {
-        gameObject.SetActive(Random.value < activeChance);
+        if (Application.isPlaying)
+        {
+            gameObject.SetActive(Random.value < activeChance);
+        }
+
+        if (!Application.isPlaying)
+        {
+            gameObject.SetActive(disableInEditor);
+        }
     }
 }
